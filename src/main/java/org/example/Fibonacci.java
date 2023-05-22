7e011ec783aa7a779f9d0a3b5cb2bb9f837d87fb
DuyenHa37513 <Duyenha37513@gmail.com>
2023-05-22 14:41:26 +0700

    inti

--- src/main/java/org/example/Fibonacci.java
+++ src/main/java/org/example/Fibonacci.java
@@ -1,4 +1,16 @@
 package org.example;
 
 public class Fibonacci {
+    public static void main(String[] args) {
+        int number1 = 0;
+        int number2 = 1;
+        System.out.println(number1);
+        System.out.println(number2);
+        for(int i = 0; i < 8; i++){
+            int number3 = number1 + number2;
+            number1 = number2;
+            number2 = number3;
+            System.out.println(number3);
+        }
+    }
 }
